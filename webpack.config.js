b3c65d950e5a05c667a24c728907d07b6c4a7781
Apostolos Papazisis <papazisisgr@hotmail.com>
2018-01-05 15:31:07 +0100

    working v1

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
+        library: 'BootstrapVue',
+        libraryTarget: 'umd'
     },
     module: {
         rules: [
