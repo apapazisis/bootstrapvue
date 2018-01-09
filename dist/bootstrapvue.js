ebb89c292d67aef51d83c668dc9619b23d019896
Apostolos Papazisis <papazisisgr@hotmail.com>
2018-01-09 23:48:54 +0100

    small fixes

--- dist/bootstrapvue.js
+++ dist/bootstrapvue.js
@@ -318,7 +318,6 @@ module.exports = function normalizeComponent (
             };
         }
     }
-
 });
 
 /***/ }),
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import alert from './components/alert.vue';
 import badge from './components/badge.vue';
 import breadcrumb from './components/breadcrumb.vue';
 
-
 const BootstrapVue = {
     alert,
     badge,
