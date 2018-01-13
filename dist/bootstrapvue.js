6b2048360c80bb0f7f782190eeeb0dfbd386864b
Apostolos Papazisis <papazisisgr@hotmail.com>
2018-01-13 14:24:36 +0100

    working on structure

--- dist/bootstrapvue.js
+++ dist/bootstrapvue.js
@@ -543,16 +543,6 @@ module.exports = function normalizeComponent (
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 
 const props = {
@@ -571,13 +561,11 @@ const props = {
 /* harmony default export */ __webpack_exports__["a"] = ({
     name: 'card-body',
     props,
-    methods: {
-        getClass(overlay) {
-            return {
-                'card-body': overlay == false,
-                'card-img-overlay': overlay == true
-            };
-        }
+    render(h) {
+        return h('p', {
+            'class': {
+                'nobody': this.noBody
+            } });
     }
 });
 
@@ -1752,14 +1740,13 @@ if (false) {(function () {
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_card_body_vue__ = __webpack_require__(8);
 /* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "b", function() { return __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_card_body_vue__["b"]; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_3296da3a_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_card_body_vue__ = __webpack_require__(30);
 var disposed = false
 var normalizeComponent = __webpack_require__(0)
 /* script */
 
 
 /* template */
-
+var __vue_template__ = null
 /* template functional */
 var __vue_template_functional__ = false
 /* styles */
@@ -1770,7 +1757,7 @@ var __vue_scopeId__ = null
 var __vue_module_identifier__ = null
 var Component = normalizeComponent(
   __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_card_body_vue__["a" /* default */],
-  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_3296da3a_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_card_body_vue__["a" /* default */],
+  __vue_template__,
   __vue_template_functional__,
   __vue_styles__,
   __vue_scopeId__,
@@ -1796,32 +1783,6 @@ if (false) {(function () {
 
 /* harmony default export */ __webpack_exports__["a"] = (Component.exports);
 
-
-/***/ }),
-/* 30 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-var render = function() {
-  var _vm = this
-  var _h = _vm.$createElement
-  var _c = _vm._self._c || _h
-  return _vm.noBody == false
-    ? _c("div", { class: _vm.getClass(_vm.overlay) }, [
-        _vm._v("\n    This is some text within a card body.\n    edo eimaste\n")
-      ])
-    : _c("div", { staticClass: "card-body" }, [_vm._t("default")], 2)
-}
-var staticRenderFns = []
-render._withStripped = true
-var esExports = { render: render, staticRenderFns: staticRenderFns }
-/* harmony default export */ __webpack_exports__["a"] = (esExports);
-if (false) {
-  module.hot.accept()
-  if (module.hot.data) {
-    require("vue-hot-reload-api")      .rerender("data-v-3296da3a", esExports)
-  }
-}
 
 /***/ })
 /******/ ]);
--- src/components/card/card-body.vue
+++ src/components/card/card-body.vue
@@ -1,13 +1,3 @@
-<template>
-    <div v-if="noBody == false" :class="getClass(overlay)">
-        This is some text within a card body.
-        edo eimaste
-    </div>
-    <div class="card-body" v-else>
-        <slot></slot>
-    </div>
-</template>
-
 <script>
 
     export const props = {
@@ -24,13 +14,11 @@
     export default {
         name: 'card-body',
         props,
-        methods: {
-            getClass(overlay) {
-                return {
-                    'card-body': overlay == false,
-                    'card-img-overlay': overlay == true
-                }
-            }
+        render(h) {
+            return h('p', {
+            'class': {
+                'nobody': this.noBody
+            }})
         }
     }
 </script>
