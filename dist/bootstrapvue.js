759f57c40453017e7b499a9ea9aaeeb70488b01d
Apostolos Papazisis <papazisisgr@hotmail.com>
2018-01-13 14:41:28 +0100

    working on structure

--- dist/bootstrapvue.js
+++ dist/bootstrapvue.js
@@ -536,7 +536,10 @@ module.exports = function normalizeComponent (
 
 /* harmony default export */ __webpack_exports__["a"] = ({
     name: 'card',
-    props: __WEBPACK_IMPORTED_MODULE_0__card_body_vue__["a" /* props */]
+    props: __WEBPACK_IMPORTED_MODULE_0__card_body_vue__["a" /* props */],
+    components: {
+        cardBody
+    }
 });
 
 /***/ }),
--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -9,6 +9,9 @@
 
     export default {
         name: 'card',
-        props: bodyProps
+        props: bodyProps,
+        components: {
+            cardBody
+        }
     }
 </script>
